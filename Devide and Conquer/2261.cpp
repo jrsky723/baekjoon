6be28f4e19623e5f38a7ffacda90964267bde646
hanu607 <hanu607@gmail.com>
2022-10-05 13:10:59 +0900

    Create 2261.cpp

---
+++ Devide and Conquer/2261.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+#define X first
+#define Y second
+vector<pii> p;
+int n;
+
+int dist(pii &p1, pii &p2) {
+  int distX = p1.X - p2.X;
+  int distY = p1.Y - p2.Y;
+  return distX * distX + distY * distY;
+}
+
+int closestDist(int st, int en) {
+  if (en - st <= 2) return dist(p[st], p[st + 1]);
+  int CDL, CDR, d;
+  int mid = (st + en) / 2;
+  int mdX = p[mid].X;
+  CDL = closestDist(st, mid);
+  CDR = closestDist(mid, en);
+  d = min(CDL, CDR);
+  vector<pii> in;
+  for (int i = st; i < en; i++) {
+    if ((p[i].X - mdX) * (p[i].X - mdX) > d) continue;
+    in.push_back(p[i]);
+  }
+  sort(in.begin(), in.end(), [](auto &a, auto &b) {
+    if (a.Y == b.Y) return a.X < b.X;
+    return a.Y < b.Y;
+  });
+  for (int i = 0; i < in.size() - 1; i++)
+    for (int j = i + 1; j < in.size(); j++) {
+      int yDist = in[j].Y - in[i].Y;
+      if (yDist*yDist >= d) break;
+      d = min(d, dist(in[i], in[j]));
+    }
+  return d;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  p.resize(n);
+  for (int i = 0; i < n; i++) cin >> p[i].X >> p[i].Y;
+  sort(p.begin(), p.end());
+  bool hasEqual = false;
+  for (int i = 0; i < n; i++) if (p[i] == p[i + 1]) hasEqual = true;
+  if (hasEqual) cout << 0;
+  else cout << closestDist(0, n);
+}
